c831f83e2001a43d6868d0f53ea5087bef1f5c7e
Shashi <>
2017-07-27 19:26:53 +0530

    Working & Moduled

---
+++ src/assign/text/Book.java
@@ -0,0 +1,54 @@
+package assign.text;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Program to get Set of Words and Their Respective Page Numbers on Which They
+ * are Written, Similar to a Glossary of any Book 
+ * Input: Set Of Valid Pages; One Exclusion-List 
+ * Output: File With List of All Words with Page Numbers
+ * @author Shashi
+ */
+public class Book {
+
+	public static void main(String[] args) throws IOException {
+
+		// Get All the Pages
+		File file = new File(BookUtil.directory);
+		List<String> allPages = new ArrayList<>();
+		try {
+			Stream.of(file.list((pFile, pString) -> pString.startsWith("page"))).forEach(allPages::add);
+		}
+		catch (Exception e) {
+			System.out.println("No Pages Found!! \nCheck The Folder Structure For Reading The Pages.");
+		}
+
+		// Get All the Necessary Words With Pages
+		LinkedHashMap<Integer, LinkedHashSet<String>> allData = BookUtil.getAllPageData(allPages);
+		List<String> words = new ArrayList<>(BookUtil.allWords);
+
+		LinkedHashMap<String, HashSet<Integer>> output = new LinkedHashMap<>();
+
+		// Finding the Matching Words and Their Count
+		for (int i = 0; i < words.size(); i++) {
+			HashSet<Integer> val = new HashSet<>();
+			for (int j = 0; j < allData.size(); j++) {
+				if (allData.get(j).toString().toLowerCase().contains(words.get(i).toLowerCase())) {
+					val.add(j + 1);
+				}
+			}
+			output.put(words.get(i), val);
+		}
+
+		// Writing to Output File in Order of Pages
+		BookUtil.writeDataToFile(output);
+	}
+
+}
---
+++ src/assign/text/BookUtil.java
@@ -0,0 +1,133 @@
+package assign.text;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * Utility Functions to Get Specific Set of Data as Per Requirement
+ * 
+ * @author Shashi
+ */
+public class BookUtil {
+
+	// Please Change This As Per Respective Directory
+	static String directory = "D:\\Workspace_New\\WordCounter\\files\\";
+
+	// To Store All Unique Words Across All Pages
+	static Set<String> allWords = new LinkedHashSet<>();
+
+	/**
+	 * @param All Page Names
+	 * @return Map With Key as Page Number and Value as Page Data
+	 * @throws IOException
+	 */
+	public static LinkedHashMap<Integer, LinkedHashSet<String>> getAllPageData(List<String> allPages)
+			throws IOException {
+
+		LinkedHashMap<Integer, LinkedHashSet<String>> allData = new LinkedHashMap<>();
+
+		// Get the List of Words to be Excluded
+		LinkedHashSet<String> exclude = getExclusionList();
+		
+		if (allPages.size() == 0) {
+			System.out.println("No Pages are provided to read!!");
+		}
+		
+		for (int i = 0; i < allPages.size(); i++) {
+			String text = new String(Files.readAllBytes(Paths.get(directory + allPages.get(i))),
+					StandardCharsets.UTF_8);
+			text = text.replaceAll("[!?,.]", "");
+			String[] tmpVals = text.split("\\s+");
+			LinkedHashSet<String> tmpVal = new LinkedHashSet<>((Arrays.asList(tmpVals)));
+
+			// Get All Relevant Words
+			LinkedHashSet<String> allText = getFilteredList(tmpVal, exclude);
+
+			// Add All Words for Further Comparison
+			allWords.addAll(allText);
+
+			// Map a Single Page With the Words Present in it
+			allData.put(i, allText);
+		}
+		return allData;
+	}
+
+	/**
+	 * @return All the Words From the Exclusion List
+	 * @throws IOException
+	 */
+	public static LinkedHashSet<String> getExclusionList() throws IOException {
+
+		String[] tmpExcl = {};
+		
+		// Read Words From The Given Exclusion-List File
+		try {
+			String excl = new String(Files.readAllBytes(Paths.get(directory + "exclusion-list.txt")),
+					StandardCharsets.UTF_8);
+			excl = excl.replaceAll("[!?,.]", "");
+			tmpExcl = excl.split("\\s+");
+		}
+		catch (Exception e) {
+			System.out.println("No Exclusion List Provided.");
+		}
+
+		LinkedHashSet<String> exclude = new LinkedHashSet<>(Arrays.asList(tmpExcl));
+
+		return exclude;
+	}
+
+	// Remove the Exclusion List Words from All the Pages
+
+	/**
+	 * @param Set of All the Words on a Page
+	 * @param List of Words for Exclusion
+	 * @return Set of All Relevant Words
+	 */
+	public static LinkedHashSet<String> getFilteredList(LinkedHashSet<String> allValues,
+			LinkedHashSet<String> exclude) {
+
+		LinkedHashSet<String> hs = allValues;
+		Iterator<String> itr = hs.iterator();
+		while (itr.hasNext()) {
+
+			String tmpVal = itr.next();
+			// Remove Irrelevant Words Comparing With The Exclusion List
+			if (exclude.contains(tmpVal)) {
+				itr.remove();
+			}
+		}
+		return hs;
+	}
+
+	/**
+	 * @param Writes Word With Pages in Which They are Occurring to a New File
+	 * @throws IOException
+	 */
+	public static void writeDataToFile(LinkedHashMap<String, HashSet<Integer>> output) throws IOException {
+
+		FileOutputStream fos = new FileOutputStream(new File("files\\index.txt"));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+		// Traverse to Get All The Words
+		for (Entry<String, HashSet<Integer>> entry : output.entrySet()) {
+			bw.write(entry.getKey() + ": " + entry.getValue().toString().replace("[", "").replace("]", ""));
+			bw.newLine();
+		}
+		bw.close();
+	}
+
+}
